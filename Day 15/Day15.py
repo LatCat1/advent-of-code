46de085cd6733fb78f6261467f30fa19284fa7f4
LatCat <lat@troan.org>
2021-12-25 12:32:48 -0500

    actually wrote dijkstra

--- Day 15/Day15.py
+++ Day 15/Day15.py
@@ -1,4 +1,5 @@
 import pathlib
+import heapq
 
 with open(f"{pathlib.Path(__file__).parent.resolve()}/Day15.txt", 'r') as file:
     lines = [line.rstrip() for line in file]
@@ -11,30 +12,28 @@ xmax = len(lines)
 ymax = len(lines[0])
 
 # for p2 ew
-# quintuplegrid = {}
-# for x in range(xmax):
-#     for y in range(ymax):
-#         for i in range(5):
-#             for i2 in range(5):
-#                 quintuplegrid[(x+i*xmax,y+i2*ymax)] = 1+ (grid[(x,y)]+i+i2-1)%9
-# grid = quintuplegrid
-# xmax *= 5
-# ymax *= 5
-def shortestpathalgo(start,grid, adjacent):
-    lastupdated = [start]
-    shortestPath = {start:0}
-    while len(lastupdated) != 0:
-        newupdated = []
-        for origin in lastupdated:
-            origincost = shortestPath[origin]
-            for next in adjacent(origin):
-                if next in grid:
-                    cost = origincost + grid[next]
-                    if next in shortestPath and cost < shortestPath[next] or next not in shortestPath:
-                        newupdated.append(next)
-                        shortestPath[next] = cost
-        lastupdated = newupdated
-    return shortestPath
+quintuplegrid = {}
+for x in range(xmax):
+    for y in range(ymax):
+        for i in range(5):
+            for i2 in range(5):
+                quintuplegrid[(x+i*xmax,y+i2*ymax)] = 1+ (grid[(x,y)]+i+i2-1)%9
+grid = quintuplegrid
+xmax *= 5
+ymax *= 5
+def shortestpathalgo(start,grid, adjacent, end):
+    heap = [(0, start)]
+    found = {}
+    while end not in found:
+        weight, loc = heapq.heappop(heap)
+        # print(weight, loc)
+        if loc not in found:
+            found[loc] = weight #add it to the 'founds'
+            for e in adjacent(loc):
+                if e in grid and e not in found:
+                    heapq.heappush(heap, (weight+grid[e], e))
+
+    return found[end]
 
 adjacent = lambda p: [(p[0]+a,p[1]+b) for (a,b) in [(1,0),(-1,0),(0,1),(0,-1)]]
-print(shortestpathalgo((0,0),grid,adjacent)[(xmax-1,ymax-1)])
+print(shortestpathalgo((0,0),grid,adjacent,(xmax-1, ymax-1)))
