fd373dc80e7fd891adc5e7ce75a1d2bd86a437f7
LatCat <lat@troan.org>
2022-12-02 23:49:47 -0600

    even cuter d3

--- 2022/Day 03/Day03.py
+++ 2022/Day 03/Day03.py
@@ -1,9 +1,8 @@
 import pathlib
 
-with open(f"{pathlib.Path(__file__).parent.resolve()}/input.txt", 'r') as file:
-    ls = [line.rstrip() for line in file]
+with open(f"{pathlib.Path(__file__).parent.resolve()}/input.txt", 'r') as f:
+    ls = f.read().split('\n')
 
 s = lambda c: ord(c) - (ord('A') - 27 if c.isupper() else ord('a') - 1)
 print(sum([s((set(l[:len(l)//2]) & set(l[len(l)//2:])).pop()) for l in ls]))
-print(sum([s((set(ls[i]) & set(ls[i+1]) & set(ls[i+2])).pop()) 
-    for i in range(0, len(ls), 3)]))
+print(sum([s((set(a) & set(b) & set(c)).pop()) for (a,b,c) in zip(*[iter(ls)]*3)]))
