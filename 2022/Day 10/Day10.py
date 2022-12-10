87b9fe905891cb71cf0aef504537332093ce7b3e
Loren Troan <lat@troan.org>
2022-12-10 00:19:56 -0600

    i fell off

---
+++ 2022/Day 10/Day10.py
@@ -0,0 +1,10 @@
+import pathlib
+
+with open(f"{pathlib.Path(__file__).parent.resolve()}/input.txt", 'r') as f:
+    lines = f.read().split('\n')
+
+in_progress = sum([[0] + ((l!='noop')*[int((l+' 0').split(' ')[1])]) for l in lines],start=[1])
+x = [sum(in_progress[0:i+1]) for i in range(240)] #repeats a lot of work :/
+
+print(sum(x[c-1]*c for c in range(20, 240, 40)))
+print(''.join('.#'[abs(x[p]-p%40) <= 1] + '\n'*(not((p+1)%40)) for p in range(240)))
