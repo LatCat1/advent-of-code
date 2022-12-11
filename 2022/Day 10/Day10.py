5687c694a4df74ad9bd0acfd1b0221dd63cec26b
Loren Troan <lat@troan.org>
2022-12-10 22:45:36 -0600

    day 10 ig

--- 2022/Day 10/Day10.py
+++ 2022/Day 10/Day10.py
@@ -7,4 +7,4 @@ in_progress = sum([[0] + ((l!='noop')*[int((l+' 0').split(' ')[1])]) for l in li
 x = [sum(in_progress[0:i+1]) for i in range(240)] #repeats a lot of work :/
 
 print(sum(x[c-1]*c for c in range(20, 240, 40)))
-print(''.join('.#'[abs(x[p]-p%40) <= 1] + '\n'*(not((p+1)%40)) for p in range(240)))
+print(''.join(' #'[abs(x[p]-p%40) <= 1] + '\n'*(not((p+1)%40)) for p in range(240)))
