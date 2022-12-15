c9cc82c55558959f972bd0733b66a9fc7fccce6f
LatCat <lat@troan.org>
2022-12-15 14:26:51 -0500

    took somebody's better idea, its better

--- 2022/Day 15/Day15.py
+++ 2022/Day 15/Day15.py
@@ -3,13 +3,14 @@ import pathlib
 with open(f"{pathlib.Path(__file__).parent.resolve()}/input.txt", 'r') as f:
     lines = f.read().split('\n')
 
+y = 2000000
+valmin = 0
+valmax = 4000000
+
 def mdist(p1, p2):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
-real = False
-
-sensors = {}
-sdists = {}
+sensors, sdists = {}, {}
 for l in lines:
     q = l.split('=')
     sx = int(q[1].split(',')[0])
@@ -19,57 +20,30 @@ for l in lines:
     sensors[(sx, sy)] = (bx, by)
     sdists[(sx, sy)] = mdist((sx, sy), (bx, by))
 
-def ycoverage(y):
-    covered = []
-    for s in sensors:
-        dist_from_sens = sdists[s] #distance for s to beacon
-        dist_from_line = mdist(s, (s[0], y))
-        if dist_from_sens >= dist_from_line:
-            extra = dist_from_sens - dist_from_line
-            covered.append((s[0]-extra, s[0] + extra))
-    covered = sorted(covered, key=lambda x:x[0])
-    new_covered = []
-    i = 0
-    while i < len(covered):
-        s = covered[i][0] # it's the start
-        e = covered[i][1]
-        while i+1 < len(covered) and covered[i+1][0] <= e:
-            e = max(covered[i+1][1], e)
-            i += 1
-        new_covered.append((s, e))
-        i += 1
-    return new_covered
 
-y = 2000000 if real else 10
-p1_coverage = ycoverage(y)
-for b in sensors.values():
-    if b[1] == y:
-        i = 0
-        while i < len(p1_coverage):
-            if p1_coverage[i][0] <= b[0] <= p1_coverage[i][1]:
-                s, e = p1_coverage.pop(i)
-                i -= 1
-                for p in [(s, b[0]-1), (b[0]+1, e)]:
-                    if p[0] <= p[1]:
-                        p1_coverage.append(p)
-            i += 1
-print('P1: ', sum(s[1] - s[0] + 1 for s in p1_coverage))
+covered = []
+for s in sensors:
+    dist_from_sens = sdists[s] #distance for s to beacon
+    dist_from_line = mdist(s, (s[0], y))
+    if dist_from_sens >= dist_from_line:
+        extra = dist_from_sens - dist_from_line
+        covered.append((s[0]-extra, s[0] + extra))
+covered = sorted(covered, key=lambda x:x[0])
+c2, i = [], 0
+while i < len(covered):
+    s, e = covered[i] # it's the start
+    while i+1 < len(covered) and covered[i+1][0] <= e:
+        e = max(covered[i+1][1], e)
+        i += 1
+    c2.append((s, e))
+    i += 1
+correction = sum(1 for b in set(sensors.values()) for r in c2 if b[1]==y and r[0]<=b[0]<=b[1])
+print('P1: ', sum(s[1] - s[0] + 1 for s in c2) - correction)
 
 # part 2
-valmin = 0
-valmax = 4000000 if real else 20
-possible = []
-for y in range(valmin, valmax+1):
-    c = ycoverage(y)
-    for i in range(len(c)-1):
-        if c[i+1][0] - c[i][1] > 1:
-            possible.append((c[i][1]+1, y))
-
-for p in possible:
-    failed = False
-    for s in sensors:
-        if mdist(s, p) <= mdist(s, sensors[s]):
-            failed=True
-    if not failed:
-        print('P2:', p[0] * 4000000 + p[1])
-        exit()
+lines = {-1: [], 1:[]}
+for d in lines:
+    lines[d] = sorted(set(s[1]-s[0]*d+sdists[s]*pm for s in sensors for pm in [-1,1]))
+    lines[d] = [lines[d][i]+1 for i in range(len(lines[d])-1) if lines[d][i]+2==lines[d][i+1]]
+point = [((n-p)//2, (n+p)//2) for p in lines[1] for n in lines[-1]][0]
+print('P2:', point[0]*4000000+point[1])
