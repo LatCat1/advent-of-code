e5301b74183f96310617275535f3b324a8208e2a
LatCat <lat@troan.org>
2022-12-16 22:22:29 -0500

    Floyd-Warshall, not speedup but for practice

--- 2022/Day 16/Day16.py
+++ 2022/Day 16/Day16.py
@@ -6,9 +6,7 @@ grid = {}
 rates = {}
 for l in lines:
     q = l.split(' ')
-    start = q[1]
-    rate = int(q[4][5:-1])
-    later = q[9:]
+    start, rate, later = q[1], int(q[4][5:-1]), q[9:]
     for l in range(len(later)-1):
         later[l] = later[l][:-1]
     later = set(later)
@@ -21,35 +19,37 @@ bitmap_guide['AA'] = len(nonzero_flow) + 1
 
 # ok we need to 'simplify' grid (maybe, this seems to make p1 slower actually). also, it doesn't help p2 
 #   because things can be 'in progress' to a location. saving how far in that progress is slower
-simplegrid = {}
-for g in set(nonzero_flow) | {'AA'}: # starting
-    # get the minimum distance from g to everything
-    min_dists_from_g = {}
-    to_check = {(0,g)}
-    while len(to_check) != 0:
-        cst, curr = min(to_check)
-        to_check.remove((cst, curr))
-        if curr not in min_dists_from_g:
-            min_dists_from_g[curr] = cst
-            to_check |= {(cst+1, n) for n in grid[curr]}
-    simplegrid[g] = {g2: min_dists_from_g[g2] for g2 in nonzero_flow - {g}}
+shortest = {n:{n:0} for n in grid}
+for n in grid:
+    for n2 in grid[n]:
+        shortest[n][n2] = 1
+ordered_nodes = list(rates.keys())
+c = 0
+for i, pivot in enumerate(ordered_nodes):
+    for a in ordered_nodes:
+        for b in ordered_nodes:
+            c += 1
+            if pivot in shortest[a] and b in shortest[pivot]:
+                detour = shortest[a][pivot] + shortest[pivot][b]
+                if b not in shortest[a] or shortest[a][b] > detour:
+                    shortest[a][b] = detour
+simplegrid = {g:{d:shortest[g][d] for d in nonzero_flow-{g}} for g in nonzero_flow | {'AA'}}
 
 all = [2**len(nonzero_flow)-1]
-mem = {}
+mem = {k:[[None]*(2**len(nonzero_flow)) for _ in range(31)] for k in nonzero_flow | {'AA'}}
 def solve(loc, time_remaining, visited):
     if visited == all[0] or time_remaining <= 0:
         return 0
-    if (loc, time_remaining, visited) in mem:
-        return mem[(loc, time_remaining, visited)]
+    if mem[loc][time_remaining][visited] is not None:
+        return mem[loc][time_remaining][visited]
     val = max((solve(loc2, time_remaining-simplegrid[loc][loc2]-1, visited | (1<<bitmap_guide[loc2]))+
             (time_remaining-simplegrid[loc][loc2]-1)*rates[loc2] for loc2 in simplegrid[loc] 
             if (visited >> bitmap_guide[loc2]) & 1 == 0), default=0)
-    mem[(loc, time_remaining, visited)] = val
+    mem[loc][time_remaining][visited] = val
     return val
 
 print('P1:', solve('AA', 30, 0))
 
-# part 2 yay. can take ~3 min to run on actual inputs
+# part 2 yay. can take ~a few minutes
 print('P2:', max(solve('AA', 26, d) + solve('AA', 26, all[0]-d) 
             for d in range(2**(len(nonzero_flow)-1))))
-print(len(mem))
