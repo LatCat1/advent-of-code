91996a06d06b870e1805e7efbec759ec98faf502
LatCat <lat@troan.org>
2022-12-17 23:53:32 -0500

    ig it works

--- 2022/Day 17/Day17.py
+++ 2022/Day 17/Day17.py
@@ -1,5 +1,4 @@
 import pathlib
-from tqdm import tqdm
 
 with open(f"{pathlib.Path(__file__).parent.resolve()}/input.txt", 'r') as f:
     jets = f.read()
@@ -50,55 +49,37 @@ def print_board(board, piece=set(), maxs=100000000000, mins=0):
     print('+-------+')
 
 loop_info = []
-def run_steps(maxstep=2022, starttime=0):
-    board = set((-1, i) for i in range(7))  # start with a row below everything
-    time=starttime
-    high_by_step = [0]
-    step = 0
-    found_loop = False
-    while step < maxstep and found_loop is False:
-        p = {(r+4+high_by_step[-1], c) for (r,c) in shapes_sets[step%5]}
-        farleft = min(p, key=lambda x:x[1])[1]
-        farright = max(p, key=lambda x:x[1])[1]
-        while True:
-            # print_board(board, p)
-            d = 1 if jets[time%len(jets)] == '>' else -1
-            jet_p = {(r, c+d) for (r,c) in p}
-            time += 1
-            # make sure it's not inside anything or off
-            if jet_p & board == set() and farleft+d>=0 and farright+d<= 6:
-                p = jet_p
-                farleft += d
-                farright += d
-            down_p = {(r-1, c) for (r,c) in p}
-            if down_p & board == set():
-                p = down_p
-            else:
-                break
-        board |= p
-        step += 1
-        newhigh = max(p, key=lambda x:x[0])[0]
-        high_by_step.append(max(newhigh, high_by_step[-1]))
-        if step % 5 == 1 and time%len(jets) == 9271:
-            r = p.pop()[0]
-            if sum((r,i) in board for i in range(7)) == 7:
-                hash = 0
-                for r2 in range(r+1, r+4):
-                    for d2 in range(0, 7):
-                        hash = (hash << 1) + (1 if (r2, d2) in board else 0)
-                loop_info.append((step, time%len(jets), high_by_step[-1], high_by_step[-1] - r, hash))
-                if len(loop_info) == 2:
-                    found_loop == True
-    # print_board(board, maxs = 100)
-    return high_by_step
-high_by_step = run_steps(100000)
-print('P1: ', high_by_step[2022])
+board = set((-1, i) for i in range(7))  # start with a row below everything
+time = 0
+high_by_step = [0]
+step = 0
+while step < 2022 or len(loop_info) < 2:
+    p = {(r+4+high_by_step[-1], c) for (r,c) in shapes_sets[step%5]}
+    farleft = min(p, key=lambda x:x[1])[1]
+    farright = max(p, key=lambda x:x[1])[1]
+    while True:
+        d = 1 if jets[time%len(jets)] == '>' else -1
+        jet_p = {(r, c+d) for (r,c) in p}
+        time += 1
+        # make sure it's not inside anything or off
+        if jet_p & board == set() and farleft+d>=0 and farright+d<= 6:
+            p = jet_p
+            farleft += d
+            farright += d
+        down_p = {(r-1, c) for (r,c) in p}
+        if down_p & board != set():
+            break
+        p = down_p
+    board |= p
+    step += 1
+    high_by_step.append(max(max(p, key=lambda x:x[0])[0], high_by_step[-1]))
+    if time%len(jets) == 0:
+        loop_info.append(step)
 
-a = loop_info[0]
-b = loop_info[1]
+print('P1:', high_by_step[2022])
 
-first_occur_step = a[0]
-second_occur_step = b[0]
+first_occur_step = loop_info[0]
+second_occur_step = loop_info[1]
 
 dist_between = second_occur_step-first_occur_step
 loop_value = high_by_step[second_occur_step] - high_by_step[first_occur_step]
@@ -109,5 +90,5 @@ starts_looping_at = total_steps - first_occur_step
 num_loops = starts_looping_at//dist_between
 extra_steps = starts_looping_at - num_loops*dist_between
 print(
-    num_loops*loop_value + high_by_step[extra_steps+first_occur_step]
+    'P2:', num_loops*loop_value + high_by_step[extra_steps+first_occur_step]
 )
