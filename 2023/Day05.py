bc4454875a4dcb6e53582740dc200664690b3e7f
Loren Troan <lt@Lorens-MacBook-Pro.local>
2023-12-05 08:09:47 -0600

    and a little bit more cleanup

--- 2023/Day05.py
+++ 2023/Day05.py
@@ -1,15 +1,38 @@
 from loader import load_data
 from functools import lru_cache, reduce
 
-data = load_data(day=5, year=2023, testing=True)
+data = load_data(day=5, year=2023, testing=False)
 data = data.split('\n\n')
 
-seeds = data[0].split(':')[1].strip().split(' ')
-curr = [int(s) for s in seeds]
+Range = (int, int)
 
-maps = data[1:]
+# A common pattern I used was sum(iter, start=[]). This 
+# just concatinates a list of lists down to a list.
 
-def pmap(m):
+# Helper functions for intervals; specifi
+######################################################
+
+# does r1 \cap r2. 
+def intersect(r1: Range, r2: Range) -> Range:
+    s1, e1 = r1
+    s2, e2 = r2
+    return (max(s1, s2), min(e1, e2))
+
+# does r1 - r2
+def diff(r1: Range, r2: Range) -> [Range]:
+    s1, e1 = r1
+    s2, e2 = r2
+    return simplify([(s1, s2), (e2, e1)])
+
+# removes all empty ranges from the list
+# possibly could merge overlaps
+def simplify(ranges: [Range]) -> [Range]:
+    return [r for r in ranges if r[1] > r[0]]
+
+# Now for parsing and applying
+######################################################
+# maps could be represented better, but this works
+def parse_map(m):
     m = m.split('\n')[1:]
     x = []
     for r in m:
@@ -17,40 +40,30 @@ def pmap(m):
         x.append((int(a), int(b), int(c)))
     return x
 
-
-def appmap(n, map):
+# This recursive structure works, and never causes an infinite loop, because
+# if we ever have an overlap, then resulting two-piece difference won't ever
+# match on that exact same overlap again. this could definitely be faster -
+# there's no need to check everything again - but this was easier to write
+def apply_map(r: Range, map) -> [Range]:
+    # try to appl
     for (dest, src, l) in map:
-        if 0 <= n - src and n - src < l:
-            return dest + n - src
-    return n
-
-# for m in maps:
-    # p = pmap(m)
-    # curr = [appmap(c, p) for c in curr]
-# print(min(curr))
-
-
-# does r1 - r2
-def diff(r1, r2):
-    s1, e1 = r1
-    s2, e2 = r2
-    return [(s1, s2), (e2, e1)]
-
-def simplify(ranges):
-    return [r for r in ranges if r[1] > r[0]]
-
-def appmap2(r, map):
-    s, e = r
-    for (dest, src, l) in map:
-        overlap = (max(s, src), min(e, src+l))
-        if overlap[1] > overlap[0]: # if nonempty:
+        overlap = intersect(r, (src, src+l))
+        if overlap[1] > overlap[0]: # if the overlap is real, split it
             rm = simplify(diff(r, overlap))
             return [(overlap[0]+dest-src, overlap[1]+dest-src)] + \
-                sum((appmap2(z, map) for z in rm), start=[])
+                   sum((apply_map(z, map) for z in rm), start=[])
+    # base case, do nothing
     return [r]
 
-rs = [(curr[i], curr[i]+curr[i+1]) for i in range(0, len(curr), 2)]
-for m in maps:
-    p = pmap(m)
-    rs = simplify(sum((appmap2(j, p) for j in rs), start=[]))
-print(min(t[0] for t in rs))
+seeds = data[0].split(':')[1].strip().split(' ')
+seeds = [int(s) for s in seeds]
+
+maps = [parse_map(m) for m in data[1:]]
+# For part 1, we just take the options as the given list - so ranges [i, i+1)
+part_1_options = [(s, s+1) for s in seeds]
+# For part 2, we read the input as a bunch of ranges (start, length)
+part_2_options = [(seeds[i], seeds[i]+seeds[i+1]) for i in range(0, len(seeds), 2)]
+for (i, opts) in enumerate([part_1_options, part_2_options]):
+    for m in maps:
+        opts = sum((apply_map(r, m) for r in opts), start=[])
+    print(f"Part {i+1}: {min(t[0] for t in simplify(opts))}")
